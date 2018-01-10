66159da758c035f8af491d8317a73a207279ac53
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-01-10 14:09:31 +1000

    Add bson_t TArray convenience functions

--- Source/ROSIntegration/Private/Conversion/Messages/BaseMessageConverter.h
+++ Source/ROSIntegration/Private/Conversion/Messages/BaseMessageConverter.h
@@ -6,6 +6,7 @@
 #include "UObject/ObjectMacros.h"
 #include "UObject/Object.h"
 #include "rosbridge2cpp/messages/rosbridge_publish_msg.h"
+#include <cstring>
 #include "bson.h"
 #include "BaseMessageConverter.generated.h"
 
@@ -51,6 +52,30 @@ public:
 			UE_LOG(LogTemp, Error, TEXT("Key %s not present in data"), *Key);
 		}
 		return value;
+	}
+
+protected:
+
+	// Helper function to append a TArray<float> to a bson_t
+	void _bson_append_float_tarray(bson_t *b, const char *key, TArray<float> tarray)
+	{
+		// float -> double doesn't loose precision
+		_bson_append_double_tarray(b, key, (TArray<double>)tarray);
+	}
+
+	// Helper function to append a TArray<double> to a bson_t
+	void _bson_append_double_tarray(bson_t *b, const char *key, TArray<double> tarray)
+	{
+		bson_t arr;
+		const char *element_key;
+		char str[16];
+		BSON_APPEND_ARRAY_BEGIN(b, key, &arr);
+		for (int i = 0; i != tarray.Num(); ++i)
+		{
+			bson_uint32_to_string(i, &element_key, str, sizeof str);
+			BSON_APPEND_DOUBLE(b, element_key, tarray[i]);
+		}
+		bson_append_array_end(b, &arr);
 	}
 
 };
