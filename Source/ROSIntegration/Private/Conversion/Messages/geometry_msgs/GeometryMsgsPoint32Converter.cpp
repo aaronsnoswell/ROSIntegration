25380255e054e7af73c30f7b6619b2a48eb41d2f
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-01-10 15:57:45 +1000

    Implement Point32 converter

--- Source/ROSIntegration/Private/Conversion/Messages/geometry_msgs/GeometryMsgsPoint32Converter.cpp
+++ Source/ROSIntegration/Private/Conversion/Messages/geometry_msgs/GeometryMsgsPoint32Converter.cpp
@@ -12,5 +12,11 @@ bool UGeometryMsgsPoint32Converter::ConvertIncomingMessage(const ROSBridgePublis
 }
 
 bool UGeometryMsgsPoint32Converter::ConvertOutgoingMessage(TSharedPtr<FROSBaseMsg> BaseMsg, bson_t** message) {
+	
+	auto Point32 = StaticCastSharedPtr<ROSMessages::geometry_msgs::Point32>(BaseMsg);
+
+	bson_init(*message);
+	_bson_append_point32(*message, Point32.Get());
+
 	return true;
 }
--- Source/ROSIntegration/Private/Conversion/Messages/geometry_msgs/GeometryMsgsPoint32Converter.h
+++ Source/ROSIntegration/Private/Conversion/Messages/geometry_msgs/GeometryMsgsPoint32Converter.h
@@ -6,6 +6,7 @@
 #include "UObject/ObjectMacros.h"
 #include "UObject/Object.h"
 #include "Conversion/Messages/BaseMessageConverter.h"
+#include "geometry_msgs/Point32.h"
 #include "GeometryMsgsPoint32Converter.generated.h"
 
 
@@ -17,6 +18,31 @@ class ROSINTEGRATION_API UGeometryMsgsPoint32Converter: public UBaseMessageConve
 public:
 	virtual bool ConvertIncomingMessage(const ROSBridgePublishMsg* message, TSharedPtr<FROSBaseMsg> &BaseMsg);
 	virtual bool ConvertOutgoingMessage(TSharedPtr<FROSBaseMsg> BaseMsg, bson_t** message);
+
+
+	// Helper function to append a ROSMessages::geometry_msgs::Point32 to an initialized bson_t
+	static void _bson_append_point32(bson_t *b, ROSMessages::geometry_msgs::Point32 *p32)
+	{
+		BSON_APPEND_DOUBLE(b, "x", p32->x);
+		BSON_APPEND_DOUBLE(b, "y", p32->y);
+		BSON_APPEND_DOUBLE(b, "z", p32->z);
+	}
+
+
+	// Helper function to append a TArray<ROSMessages::geometry_msgs::Point32> to an initialized bson_t
+	static void _bson_append_point32_tarray(bson_t *b, const char *key, TArray<ROSMessages::geometry_msgs::Point32> tarray)
+	{
+		bson_t arr;
+		const char *element_key;
+		char str[16];
+		BSON_APPEND_ARRAY_BEGIN(b, key, &arr);
+		for (int i = 0; i != tarray.Num(); ++i)
+		{
+			bson_uint32_to_string(i, &element_key, str, sizeof str);
+			_bson_append_point32(b, &(tarray[i]));
+		}
+		bson_append_array_end(b, &arr);
+	}
 
 };
 
