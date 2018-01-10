013688d98795729ab471f2c928a7fa910ed0a86b
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-01-10 15:05:04 +1000

    Refactor and add _bson_append_pointfield_tarray helper function

--- Source/ROSIntegration/Private/Conversion/Messages/sensor_msgs/SensorMsgsPointFieldConverter.h
+++ Source/ROSIntegration/Private/Conversion/Messages/sensor_msgs/SensorMsgsPointFieldConverter.h
@@ -6,6 +6,7 @@
 #include "UObject/ObjectMacros.h"
 #include "UObject/Object.h"
 #include "Conversion/Messages/BaseMessageConverter.h"
+#include "sensor_msgs/PointField.h"
 #include "SensorMsgsPointFieldConverter.generated.h"
 
 
@@ -17,6 +18,32 @@ class ROSINTEGRATION_API USensorMsgsPointFieldConverter: public UBaseMessageConv
 public:
 	virtual bool ConvertIncomingMessage(const ROSBridgePublishMsg* message, TSharedPtr<FROSBaseMsg> &BaseMsg);
 	virtual bool ConvertOutgoingMessage(TSharedPtr<FROSBaseMsg> BaseMsg, bson_t** message);
+
+
+	// Helper function to append a ROSMessages::sensor_msgs::PointField to an initialized bson_t
+	static void _bson_append_pointfield(bson_t *b, ROSMessages::sensor_msgs::PointField *pf)
+	{
+		BSON_APPEND_UTF8(b, "name", TCHAR_TO_UTF8(*pf->name));
+		BSON_APPEND_INT32(b, "offset", pf->offset);
+		BSON_APPEND_INT32(b, "datatype", (uint8)(pf->datatype));
+		BSON_APPEND_INT32(b, "count", pf->count);
+	}
+
+
+	// Helper function to append a TArray<ROSMessages::sensor_msgs::PointField> to a bson_t
+	static void _bson_append_pointfield_tarray(bson_t *b, const char *key, TArray<ROSMessages::sensor_msgs::PointField> tarray)
+	{
+		bson_t arr;
+		const char *element_key;
+		char str[16];
+		BSON_APPEND_ARRAY_BEGIN(b, key, &arr);
+		for (int i = 0; i != tarray.Num(); ++i)
+		{
+			bson_uint32_to_string(i, &element_key, str, sizeof str);
+			_bson_append_pointfield(b, &(tarray[i]));
+		}
+		bson_append_array_end(b, &arr);
+	}
 
 };
 
