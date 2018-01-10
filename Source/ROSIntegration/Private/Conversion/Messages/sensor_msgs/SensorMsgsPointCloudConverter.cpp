eebaf838ab0b57ab6ab3513c5cc2f64f0cd79022
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-01-10 15:58:25 +1000

    Add sensor_msgs/PointCloud

---
+++ Source/ROSIntegration/Private/Conversion/Messages/sensor_msgs/SensorMsgsPointCloudConverter.cpp
@@ -0,0 +1,36 @@
+#include "Conversion/Messages/sensor_msgs/SensorMsgsPointCloudConverter.h"
+#include "Conversion/Messages/geometry_msgs/GeometryMsgsPoint32Converter.h"
+#include "Conversion/Messages/sensor_msgs/SensorMsgsChannelFloat32Converter.h"
+
+#include "sensor_msgs/PointCloud.h"
+
+
+USensorMsgsPointCloudConverter::USensorMsgsPointCloudConverter(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	_MessageType = "sensor_msgs/PointCloud";
+}
+
+bool USensorMsgsPointCloudConverter::ConvertIncomingMessage(const ROSBridgePublishMsg* message, TSharedPtr<FROSBaseMsg> &BaseMsg) {
+	return false;
+}
+
+bool USensorMsgsPointCloudConverter::ConvertOutgoingMessage(TSharedPtr<FROSBaseMsg> BaseMsg, bson_t** message) {
+
+	auto PointCloud = StaticCastSharedPtr<ROSMessages::sensor_msgs::PointCloud>(BaseMsg);
+
+	assert(PointCloud->points.Num() >= 1);
+	assert(PointCloud->channels.Num() >= 1);
+
+	bson_init(*message);
+
+	_bson_append_header(*message, PointCloud->header);
+	BSON_APPEND_INT32(*message, "seq", PointCloud->seq);
+	BSON_APPEND_INT32(*message, "step", PointCloud->step);
+	BSON_APPEND_UTF8(*message, "frame_id", TCHAR_TO_UTF8(*PointCloud->frame_id));
+	UGeometryMsgsPoint32Converter::_bson_append_point32_tarray(*message, "points", PointCloud->points);
+	USensorMsgsChannelFloat32Converter::_bson_append_channelfloat32_tarray(*message, "channels", PointCloud->channels);
+	
+	return true;
+
+}
