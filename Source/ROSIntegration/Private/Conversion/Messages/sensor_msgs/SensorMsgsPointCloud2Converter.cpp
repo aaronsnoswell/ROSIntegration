de8ffb6dfd7e7582a1ec3d233c2dff824d86d09b
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-01-10 15:05:23 +1000

    Add sensor_msgs/PointCloud2 converter

---
+++ Source/ROSIntegration/Private/Conversion/Messages/sensor_msgs/SensorMsgsPointCloud2Converter.cpp
@@ -0,0 +1,38 @@
+#include "Conversion/Messages/sensor_msgs/SensorMsgsPointCloud2Converter.h"
+
+#include "sensor_msgs/PointCloud2.h"
+#include "Conversion/Messages/sensor_msgs/SensorMsgsPointFieldConverter.h"
+
+
+USensorMsgsPointCloud2Converter::USensorMsgsPointCloud2Converter(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	_MessageType = "sensor_msgs/PointCloud2";
+}
+
+bool USensorMsgsPointCloud2Converter::ConvertIncomingMessage(const ROSBridgePublishMsg* message, TSharedPtr<FROSBaseMsg> &BaseMsg) {
+	return false;
+}
+
+bool USensorMsgsPointCloud2Converter::ConvertOutgoingMessage(TSharedPtr<FROSBaseMsg> BaseMsg, bson_t** message) {
+
+	auto PointCloud2 = StaticCastSharedPtr<ROSMessages::sensor_msgs::PointCloud2>(BaseMsg);
+
+	assert(PointCloud2->fields.Num() >= 1);
+	assert(PointCloud2->data.Num() >= 1);
+
+	bson_init(*message);
+
+	_bson_append_header(*message, PointCloud2->header);
+	BSON_APPEND_INT32(*message, "height", PointCloud2->height);
+	BSON_APPEND_INT32(*message, "width", PointCloud2->width);
+	USensorMsgsPointFieldConverter::_bson_append_pointfield_tarray(*message, "fields", PointCloud2->fields);
+	BSON_APPEND_BOOL(*message, "is_bigendian", PointCloud2->is_bigendian);
+	BSON_APPEND_INT32(*message, "point_step", PointCloud2->point_step);
+	BSON_APPEND_INT32(*message, "row_step", PointCloud2->row_step);
+	_bson_append_uint8_tarray(*message, "data", PointCloud2->data);
+	BSON_APPEND_BOOL(*message, "is_dense", PointCloud2->is_dense);
+		
+	return true;
+
+}
